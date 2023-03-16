e7dfa2c63606fd18a43f274498db701b4417f99c
WinCodeX <glenwinterg970@gmail.com>
2023-03-16 09:48:04 +0300

    Added Navbar & Footer

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,5 @@ class Account < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
+  has_many :properties
 end
